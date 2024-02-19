15c184850ead58b6c45a1996073f7d25a5bd5c14
baljeet0298 <71583691+baljeet0298@users.noreply.github.com>
2024-02-19 22:25:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 import streamlit as st
 import time
 import beepy as beep
-
+from threading import Thread
 import os
+def play1():
+  beep.beep(1)
+def play3():
+  beep.beep(3)
 def count_down(ts):
   while ts:
     st.write(ts)
     if ts<4:
-      beep.beep(3)
+      s_task=Thread(target=play3)
     else:
-      beep.beep(1)
+      s_task=Thread(target=play1)
+    s_task.start()
     time.sleep(0.5)
     ts -= 1  
   st.header("Time Up!")
